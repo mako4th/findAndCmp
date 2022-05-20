a7f9f544df1b5186684be9441fcd3b5275ffd22d
mako4th <mako4th@snow.plala.or.jp>
2022-05-20 22:01:47 +0900

    update

--- findAndCmp.sh
+++ findAndCmp.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 #Find files and sort.
-types=("jpg" "png")
-for type in ${types[@]}
-do
-    find $1 -type f -name "*.${type}" -ls >>tmp
-done
+#IFS=$'\n'
 
-sort -n -k1 tmp>tmp2
+#types=("jpg")
+#for type in ${types[@]}; do
+find $1 -type f -ls | cut -w -f7- >tmp
+#done
+
+cat tmp | cut -w -f 1 |  sort -n -k1 | uniq >tmp2 
+
+while read size;do
+  i=0
+  j=0
+  line=($(grep ^${size} tmp))
+  echo "${line[@]}"
+done <tmp2
+
